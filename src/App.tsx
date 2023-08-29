ece07b06cf5c3662ca578f2d78f5dcae44d167ca
Sikyin Tang <thomas2d19@gmail.com>
2023-08-29 17:53:01 -0400

    Allow user to create more tasks
    Add a text input field and a button to allow user to type the task
    name and add to the to-do list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from "react";
+import { ChangeEventHandler, FC, useCallback, useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoItem from "./models/todo";
 
@@ -12,6 +12,20 @@ const App: FC = () => {
   const [todoList, setTodoList] = useState<Array<TodoItem>>(
     defaultTodoItems.map((name) => ({ name, status: "TODO" })),
   );
+
+  const [taskInput, setTaskInput] = useState("");
+
+  const onTaskInputChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    (event) => {
+      setTaskInput(event.currentTarget.value);
+    },
+    [setTaskInput],
+  );
+
+  const addItem = useCallback(() => {
+    setTodoList((prev) => prev.concat([{ name: taskInput.trim(), status: "TODO" }]));
+    setTaskInput("");
+  }, [taskInput, setTaskInput, setTodoList]);
 
   const removeItem = useCallback(
     (index: number) => {
@@ -38,6 +52,33 @@ const App: FC = () => {
   return (
     <div className="container justify-content-center">
       <h1 className="text-center mb-4">Simple To-Do</h1>
+      <div className="row">
+        <div className="col col-md-auto d-flex align-items-center justify-content-end">
+          <p className="m-0 text-end">Add a new task:</p>
+        </div>
+        <div className="col d-flex align-items-center">
+          <input
+            className="form-control"
+            type="text"
+            onChange={onTaskInputChange}
+            value={taskInput}
+          />
+        </div>
+        <div className="col col-md-auto d-flex align-items-center justify-content-center my-1">
+          <button
+            className="btn btn-primary"
+            disabled={taskInput.trim().length === 0}
+            onClick={addItem}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col">
+          <h3 className="mt-5">Task list:</h3>
+        </div>
+      </div>
       <TodoList
         todoList={todoList}
         removeItem={removeItem}
